a8254ad3b599c105b17e72f53bd9a79fee7b3171
FatmaNader <fatma.nader@outlook.com>
2018-06-29 12:30:17 +0200

    test2

--- app.js
+++ app.js
@@ -1,24 +1,40 @@
+//  this is the server
+
+//const PORT = process.env.PORT || 3000;
+var PORT =3000;
+//require all dependencies.
+var debug = require('debug')('MQTT:server');
+var http = require('http');
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var index = require ('./routes/index');
+var app = express(),
+http = require('http'),
+busboy = require("then-busboy");
 
-var app = express();
+//body parser middle ware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}));
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
-.set('views', path.join(__dirname, 'views'))
-.set('view engine', 'ejs')
-app.use('/', require('./routes/index'));
+//adjust views and ejs
+app.set('views',path.join(__dirname,'views'));
+app.set('view engine','ejs');
+app.engine('html',require('ejs').renderFile);
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+app.use('/', require('./routes/index.js'));
+
+index(app);
+
+
+
+module.exports = app;
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -26,21 +42,6 @@ app.use(function(req, res, next) {
   err.status = 404;
   next(err);
 });
-
-//
-
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
-
-
-//
-
-
-
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
@@ -51,6 +52,3 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
-
-
-module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,36 +1,51 @@
-var mqtt = require('mqtt'), url = require('url');
-// Parse
-var mqtt_url = url.parse(process.env.CLOUDMQTT_URL || 'mqtt://localhost:1883');
-var auth = (mqtt_url.auth || ':').split(':');
-var router = express.Router();
+var express = require('express');
+// var mqtt = require('mqtt'), url = require('url');
+// // Parse
+// var mqtt_url = url.parse(process.env.CLOUDMQTT_URL || 'mqtt://localhost:1883'); //	m12.cloudmqtt.com
+// var auth = (mqtt_url.auth || ':').split(':');
+// //var router = express.Router();
+ module.exports = function(app) {
+// // Create a client connection
+// var client = mqtt.connect(mqtt_url);
+//
+// client.on('connect', function() { // When connected
+//
+//   // subscribe to a topic
+//   client.subscribe('hello/world', function() {
+//     // when a message arrives, do something with it
+//     client.on('message', function(topic, message, packet) {
+//       console.log("Received '" + message + "' on '" + topic + "'");
+//     });
+//   });
+//
+//   // publish a message to a topic
+//   client.publish('hello/world', 'my message', function() {
+//     console.log("Message is published");
+//     client.end(); // Close the connection when published
+//   });
+// });
 
-// Create a client connection
-var client = mqtt.createClient(mqtt_url.port, mqtt_url.hostname, {
-  username: auth[0],
-  password: auth[1]
+
+
+
+
+// var mqtt = require('mqtt')
+// var client  = mqtt.connect('mqtt://test.mosquitto.org')
+//
+// client.on('connect', function () {
+//   client.subscribe('presence')
+//   client.publish('presence', 'Hello mqtt')
+// })
+//
+// client.on('message', function (topic, message) {
+//   // message is Buffer
+//   console.log(message.toString())
+//   client.end()
+// })
+
+app.get('/', function(req, res, next) {
+  //var config =  url.parse(mqtt_url);
+//  config.topic = topic;
+  res.render('index.ejs');
 });
-
-client.on('connect', function() { // When connected
-
-  // subscribe to a topic
-  client.subscribe('hello/world', function() {
-    // when a message arrives, do something with it
-    client.on('message', function(topic, message, packet) {
-      console.log("Received '" + message + "' on '" + topic + "'");
-    });
-  });
-
-  // publish a message to a topic
-  client.publish('hello/world', 'my message', function() {
-    console.log("Message is published");
-    client.end(); // Close the connection when published
-  });
-});
-router.get('/', function(req, res, next) {
-  var config =  url.parse(mqtt_url);
-  config.topic = topic;
-  res.render('index', {
-	connected: client.connected,
-	config: config
-  });
-});
+}
